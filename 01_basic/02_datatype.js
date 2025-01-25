506b67577eaaa51e2e742391a98e8d2e9d509ea0
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-01-25 09:28:08 +0000

    datatype complete

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all Js code as newer version
+
+//alert("hello") //we are using notejs not browser
+
+console.log(3+3);
+
+//Code readability should be high
+console.log("Pratik");
+
+let name = "Pratik" 
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => representation of empty value
+// symbol => unique 
+
+//object 
+
+console.log(typeof "Pratik");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined)
+
+
+
+
+
+
+
