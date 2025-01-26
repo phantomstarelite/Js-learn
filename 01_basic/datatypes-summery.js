0b22560f17551728d537803b138f23264cda1d79
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-01-26 18:31:55 +0000

    completed  datatype summery

---
+++ 01_basic/datatypes-summery.js
@@ -0,0 +1,39 @@
+// #Primitive 
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol ,BigInt
+
+// Js is Dynamically type 
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = true
+const outsideTemp = null
+// let userEmail = undefined
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3455548878798748777n
+
+
+// Reference type (Non Primitive)
+
+// Array, Objects, Functions
+const heros = ["iron-man", "hulk", "captain-america"]
+let myob = {
+    name: "Pratik",
+    age: 20,
+}
+
+const myFunction = function()
+{
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+
+
