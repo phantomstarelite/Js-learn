71f608fbb68db9b809b4aef8f70cf2d4854de633
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-01-26 18:08:17 +0000

    completed comparison operator

---
+++ 01_basic/05_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//hum aise comparision to avoid kar te hai
+
+console.log("2" === 2);
+
+
+
