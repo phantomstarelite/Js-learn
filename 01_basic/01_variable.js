b1f4f0eeed8be5c15c2f209f73e953f59abd6c3e
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-02-09 23:54:25 +0000

    01_variable file is updated

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -15,4 +15,4 @@ console.log(accountId);
 /*
 Prefer not to use var because of issue in block scope and function scope
 */
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountEmail, accountId, accountPassword])
