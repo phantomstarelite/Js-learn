57b44a6d4ed001d260ffe85f474842319ff42a04
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-02-10 00:40:53 +0000

    file is upadated again

--- 01_basic/07_numsandmath.js
+++ 01_basic/07_numsandmath.js
@@ -34,4 +34,4 @@ const hundreds = 100000
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)              
