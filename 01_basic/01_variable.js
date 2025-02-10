ad1f31883ba0b9fda322634358927ce8cce292cb
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-02-10 00:28:41 +0000

    table 2 is added is 01_variable.js

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -16,3 +16,5 @@ console.log(accountId);
 Prefer not to use var because of issue in block scope and function scope
 */
 console.table([accountEmail, accountId, accountPassword])
+
+console.table([accountCity, accountState])
