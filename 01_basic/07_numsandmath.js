f88689195fbec1abf45e57241fbf817b8dfde9ba
Pratik Khumkar <123758451+phantomstarelite@users.noreply.github.com>
2025-02-10 00:40:41 +0000

    07_numsandmath.js file is updated

--- 01_basic/07_numsandmath.js
+++ 01_basic/07_numsandmath.js
@@ -28,6 +28,8 @@ const hundreds = 100000
 // console.log(Math.random());
 // console.log((Math.random()*10) + 1);
 // console.log(Math.floor((Math.random()*10)+1))
+// console.log(Math.floor(Math.random()*10));
+
 
 const min = 10
 const max = 20
